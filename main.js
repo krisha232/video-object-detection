cb51876ba273d0ba39add2db874e22462bd5f717
krisha232 <78255450+krisha232@users.noreply.github.com>
2021-12-28 17:13:08 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 video="";
 status1="";
+objects=[];
 
 function preload(){
     video=createVideo('video.mp4');
@@ -13,6 +14,28 @@ function setup(){
 
 function draw(){
     image(video,0,0,480,380);
+    if(status1 !=""){
+        objectDetector.detect(video, gotResult);
+        for(i=0; i<objects.length; i++){
+            document.getElementById("status").innerHTML="Status : Objects Detected";
+            document.getElementById("number").innerHTML="Number of objects detected are :"  + objects.length;
+
+            fill("#FF0000");
+            percent=floor(objects[i].confidence * 100);
+            text(objects[i].label + ""+percent+"%",objects[i].x + 15, objects[i].y+15);
+            noFill();
+            stroke("#FF0000");
+            rect(objects[i].x,objects[i].y,objects[i].width,objects[i].height);
+        }
+    }
+}
+
+function gotResult(error,results){
+    if(error){
+        console.log(error);
+    }
+    console.log(results);
+    objects=results;
 }
 
 function start(){
